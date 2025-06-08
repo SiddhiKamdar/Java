2194a1ef299005f4639235f3aa832d57666b8749
SiddhiKamdar <siddhikamdar00@gmail.com>
2025-06-08 15:06:07 +0530

    Updated .gitignore -Demo of Spring App -Created a Simple Rest Controller Application

---
+++ Spring & Spring Boot/RestApp/src/main/java/com/example/RestApp/Controller/MyController.java
@@ -0,0 +1,20 @@
+package com.example.RestApp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  
+@RequestMapping("/")
+public class MyController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World!!";
+	}
+	
+	@GetMapping("/bonjour")
+	public String sayBonjour() {
+		return "Bonjour Le Monde!!";
+	}
+}
