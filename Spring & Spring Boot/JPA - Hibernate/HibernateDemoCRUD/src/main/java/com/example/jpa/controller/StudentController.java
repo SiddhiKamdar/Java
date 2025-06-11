e8e8e5e396926fab0f8c681201962d4d754d9fd7
SiddhiKamdar <siddhikamdar00@gmail.com>
2025-06-11 11:58:48 +0530

    -Hibernate/JPA CRUD->Read Done

--- Spring & Spring Boot/JPA - Hibernate/HibernateDemoCRUD/src/main/java/com/example/jpa/controller/StudentController.java
+++ Spring & Spring Boot/JPA - Hibernate/HibernateDemoCRUD/src/main/java/com/example/jpa/controller/StudentController.java
@@ -1,6 +1,10 @@
 package com.example.jpa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,22 @@ public class StudentController {
 	@Autowired
 	private StudentDao studentDao;
 	
+	//CRUD - create
 	@PostMapping("/students")
 	public String addStudent(@RequestBody Student student) {
 		studentDao.save(student);
 		return "Student Saved Successfully!!";
+	}
+	
+	//CRUD -reading a single object
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable Integer studentId) {
+		return studentDao.findById(studentId);
+	}
+	
+	//CRUD - read all objects
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		return studentDao.findAll();
 	}
 }
--- Spring & Spring Boot/JPA - Hibernate/HibernateDemoCRUD/src/main/java/com/example/jpa/dao/StudentDaoImpl.java
+++ Spring & Spring Boot/JPA - Hibernate/HibernateDemoCRUD/src/main/java/com/example/jpa/dao/StudentDaoImpl.java
@@ -1,11 +1,14 @@
 package com.example.jpa.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.example.jpa.entity.Student;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 
 @Repository
@@ -18,6 +21,18 @@ public class StudentDaoImpl implements StudentDao{
 	@Transactional
 	public void save(Student student) {
 		entityManager.persist(student);
+	}
+
+	@Override
+	public Student findById(Integer id) {
+		return entityManager.find(Student.class, id);
+	}
+
+	@Override
+	public List<Student> findAll() {
+		TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);		
+		List<Student> students = query.getResultList();
+		return students;
 	}
 
 }
