23d329748c5dd42bde26cca3f1f41c19a4c84cf0
SiddhiKamdar <siddhikamdar00@gmail.com>
2025-06-11 10:48:05 +0530

    -Hibernate/JPA CRUD->Create Done

---
+++ Spring & Spring Boot/JPA - Hibernate/HibernateDemoCRUD/src/main/java/com/example/jpa/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.jpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jpa.dao.StudentDao;
+import com.example.jpa.entity.Student;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	private StudentDao studentDao;
+	
+	@PostMapping("/students")
+	public String addStudent(@RequestBody Student student) {
+		studentDao.save(student);
+		return "Student Saved Successfully!!";
+	}
+}
